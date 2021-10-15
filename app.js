1ee8e310fe56cf7b45f0c0ab1271d8d87faa4fb7
SaaD_04 <shaad036@gmail.com>
2021-10-15 14:29:56 +0600

    route with api solved

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', authMiddleware, spendingRouter);
-app.use('/api/auth', authRouter);
+app.use('/auth', authRouter);
 
 
 app.listen(PORT, () => {
